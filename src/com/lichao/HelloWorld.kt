9e251c1f07c3f96c1c8a8199063248a318932c6d
lichao3140 <396229938@qq.com>
2017-12-05 23:07:38 +0800

    Initial commit

---
+++ src/com/lichao/HelloWorld.kt
@@ -0,0 +1,20 @@
+package com.lichao
+
+/**
+ * 常量定义： val 常量名（任意文字、长度不限）
+ * 变量定义:  var
+ * 整数型： var 变量名 : Int = 3 也可以 var : 3
+ * 浮点型: 默认是Double（双精度）
+ */
+val PI = 3.1415
+val 工资 = 500
+var money = 100
+
+fun main(args: Array<String>) {
+    println("常量：$PI")
+    println("常量：$工资")
+    money = money * 5;
+    println("变量：$money")
+    
+}
+
